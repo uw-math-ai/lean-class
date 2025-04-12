f4043b2c76de9fafb3ab9fc1d94daaf73396aa22
Vasily Ilin <vasilin97@gmail.com>
2025-04-12 00:22:56 -0700

    use `sorry` in the original hw.

--- LeanClass/1.lean
+++ LeanClass/1.lean
@@ -5,117 +5,70 @@ import Mathlib.Tactic
 
 -- 1
 example : 3 + 2 = 5 :=
-  rfl
+  sorry
 
 -- 2
 --Useful lemmas
 #check mul_comm --a * b = b * a
 #check mul_assoc --a * b * c = a * (b * c)
 example (a b c : ℝ) : a * b * c = (a * b) * c := by
-  rfl
+  sorry
 
 -- 3
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  rw [←mul_assoc]
-  rw [mul_comm a b]
-  rw [mul_assoc]
+  sorry
 
 -- 4
 example (a b c d e f : ℝ) (h : b * c = e * f) : a * b * c * d = a * e * f * d := by
-  rw [mul_assoc a b c]
-  rw [h]
-  rw [mul_assoc a e f]
+  sorry
 
 #check add_assoc
 -- 5
 --Please do this exercise without using the ring tactic.
 example (a b : ℝ) : a^3 - b^3 = (a-b)*(a^2+a*b+b^2) := by
-  rw [pow_three, pow_three]
-  rw [mul_add, mul_add]
-  repeat rw [sub_mul]
-  rw [← mul_assoc a a b, ← mul_assoc b a b]
-  rw [mul_comm b a]
-  repeat rw [pow_two]
-  repeat rw [mul_comm b (a*a)]
-  repeat rw [sub_eq_add_neg (a*a*b) (a*b*b)]
-  rw [sub_eq_add_neg (a*(a*a)) (a*a*b)]
-  repeat rw [← mul_assoc]
-  simp [add_assoc]
-  repeat rw [sub_eq_add_neg]
-  repeat simp [← add_assoc]
+  sorry
 
 -- 6: Product of two odd numbers is odd.
 example : ∀ m n : Nat, Odd m ∧ Odd n → Odd (m * n) := by
-  intro m n c
-  cases c with
-  | intro left right =>
-    unfold Odd
-    unfold Odd at left
-    unfold Odd at right
-    obtain ⟨tl, hm⟩ := left
-    obtain ⟨kp, hn⟩ := right
-    rw [hm, hn]
-    -- rw [mul_add, add_mul, right_distrib]
-    -- ring_nf
-    use tl+tl*kp*2+kp
-    ring
+  sorry
 
 -- 7
 -- Useful lemma
 #check le_trans -- a ≤ b → b ≤ c → a ≤ c
 --Please do not use the linarith tactic
 example (x y z w: ℝ) (h1 : x ≤ y) (h2 : y ≤ z) (h3: z ≤ w) : x ≤ w := by
-  apply le_trans h1 (le_trans h2 h3)
+  sorry
 
 -- 8
 variable (P Q R : Prop)
 example : (P → Q → R) → (P → Q) → P → R := by
-  intro hPQR
-  intro hPQ
-  intro hP
-  exact (hPQR hP) (hPQ hP)
+  sorry
 
 -- 9
 example : False → P := by
-  intro hp
-  exfalso
-  exact hp
+  sorry
 
 -- 10
 example : P → ¬¬P := by
-  intro hP hnP
-  exact hnP hP
+  sorry
 
 
 -- 11
 example : P ∧ Q → Q := by
-  intro h
-  cases' h with hP hQ
-  exact hQ
+  sorry
 
 -- 12
 example : (P → Q → R) → P ∧ Q → R := by
-  intro hPQR hPQ
-  cases' hPQ with hP hQ
-  exact (hPQR hP hQ)
+  sorry
 
 -- 13
 example : ¬(P ∧ Q) ↔ ¬P ∨ ¬Q := by
-  -- constructor
-  -- intro h
-  tauto
+  sorry
 
 -- 14
 example : ∀ x : ℝ, ∃ y, x + y = 2 := by
-  intro x
-  use -x+2
-  simp
-  -- rw [← add_assoc]
-  -- rw [add_comm x]
-  -- rw[neg_add_cancel]
-  -- rw[zero_add]
+  sorry
 
 -- 15
 example : ∃ x : ℝ, 3 * x + 7 ≠ 12 := by
-  use 0
-  simp
+  sorry
