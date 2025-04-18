bbae1dafd1d86c651290ed21b7b5312d3ce8e3f7
vilin97 <vasilin97@gmail.com>
2025-04-17 23:51:50 -0700

    Downgrade to 4.18

--- LeanClass/canonical.lean
+++ LeanClass/canonical.lean
@@ -4,6 +4,9 @@ import Canonical
 def add_assoc (a b c : Nat) : (a + b) + c = a + (b + c) :=
   by canonical
 
+def add_assoc' (a b c d e : Nat) : (a + b) + c + d + e = a + d + e + (b + c) :=
+  by canonical 10
+
 -- enumerate over elements of a type
 example : List Nat := by canonical (count := 10)
 
